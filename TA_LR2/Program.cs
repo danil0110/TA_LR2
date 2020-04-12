b744d84992e90a700a29af91832a5d9b6da9df6f
danil0110 <danilmail0110@gmail.com>
2020-04-12 14:54:42 +0300

    Repo is created

---
+++ TA_LR2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TA_LR2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
